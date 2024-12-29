c695f6191de67bef0d331bd434c5c1666455d48f
Daria N <gbfdariagbfnap@gmail.com>
2024-12-29 11:51:26 -0500

    HW6

--- features/steps/target_cart_page.py
+++ features/steps/target_cart_page.py
@@ -1,69 +1,22 @@
-from behave import given, when, then
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from time import sleep
-
 from behave import given, when, then
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
-
-# Set up Selenium
-service = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service)
-driver.maximize_window()
-
-# Step Definitions
-
-@given('I open the Target main page')
-def open_main_page(context):
-    context.driver = driver
-    context.driver.get('https://www.target.com/')
-    WebDriverWait(context.driver, 10).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, '.sc-e487bf3b-1.iZzjLF'))
-    )
-
-@when('Search for mug')
-def search_for_mug(context):
-    search_box = context.driver.find_element(By.ID, "search")
-    search_box.send_keys("mug")
-    search_box.send_keys(Keys.RETURN)
-    WebDriverWait(context.driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, "//button[contains(text(), 'Add to cart')]"))
-    )
-
-@when('Click on Add to Cart button')
-def click_add_to_cart(context):
-    add_to_cart_button = context.driver.find_element(By.XPATH, "(//button[contains(text(), 'Add to cart')])[1]")
-    add_to_cart_button.click()
-    WebDriverWait(context.driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, "//button[contains(text(), 'View cart & check out')]"))
-    )
-
-@when('Confirm Add to Cart button from side navigation')
-def confirm_add_to_cart(context):
-    confirm_button = context.driver.find_element(By.XPATH, "//button[contains(text(), 'View cart & check out')]")
-    confirm_button.click()
-    WebDriverWait(context.driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, "//a[@aria-label='Cart']"))
-    )
 
 @when('Open cart page')
-def open_cart_page(context):
-    cart_icon = context.driver.find_element(By.XPATH, "//a[@aria-label='Cart']")
-    cart_icon.click()
-    WebDriverWait(context.driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, "//span[contains(text(), '1 item')]"))
-    )
+def open_cart(context):
+    context.driver.get('https://www.target.com/cart')
 
-@then('I should see cart has 1 item(s)')
-def verify_cart_item_count(context):
-    cart_count = context.driver.find_element(By.XPATH, "//span[contains(text(), '1 item')]")
-    assert "1 item" in cart_count.text, "Cart does not contain 1 item."
-    context.driver.quit()
+@then('Verify cart has {amount} item(s)')
+def verify_cart_items(context, amount):
+    cart_summary = context.driver.find_element(By.XPATH, "//div[./span[contains(text(), 'subtotal')]]").text
+    assert f'{amount} item' in cart_summary, f"Expected {amount} items but got {cart_summary}"
+
+@then('Verify cart has correct product')
+def verify_product_name(context):
+    actual_name = context.driver.find_element(By.CSS_SELECTOR, "[data-test='cartItem-title']").text
+    print(f'Actual product in cart name: {actual_name}')
+    print(f'Product name stored earlier: {context.product_name}')
+    assert context.product_name in actual_name, f"Expected {context.product_name} but got {actual_name}"
+
+@then("Verify 'Your cart is empty' message is shown")
+def verify_cart_empty(context):
+    context.app.cart_page.verify_cart_empty()
--- features/tests/product_search.feature
+++ features/tests/product_search.feature
@@ -6,3 +6,31 @@ Feature: Test Scenarios for Search functionality
     When Input Car into search field
     And Click on search icon
     Then Product results for Car are shown
+
+Feature: Tests for search
+  Scenario: User can search for tea
+    Given Open target main page
+    When Search for tea
+    Then Verify search results shown for tea
+  Scenario: User can search for coffee
+    Given Open target main page
+    When Search for coffee
+    Then Verify search results shown for coffee
+  Scenario: User can search for a mug
+    Given Open target main page
+    When Search for a mug
+    Then Verify search results shown for a mug
+  Scenario Outline: User can search for a product
+    Given Open target main page
+    When Search for <product>
+    Then Verify search results shown for <product>
+    Examples:
+    |product    |
+    |coffee     |
+    |tea        |
+    |mug        |
+
+  Scenario: Verify that user can see product names and images
+    Given Open target main page
+    When Search for AirPods (3rd Generation)
+    Then Verify that every product has a name and an image
